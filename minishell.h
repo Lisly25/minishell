d65425a70d7e9258c6fe2d904b70558a0b60bfd7
Szabina Korbai <skorbai@c1r3p3.hive.fi>
2024-02-22 15:30:24 +0200

    Included the libft.h and parse.h headers

--- minishell.h
+++ minishell.h
@@ -3,24 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fshields <fshields@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:17:46 by fshields          #+#    #+#             */
-/*   Updated: 2024/02/20 11:01:39 by fshields         ###   ########.fr       */
+/*   Updated: 2024/02/22 14:50:18 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-void rl_replace_line (const char *, int);
-void rl_clear_history (void);
-
+# include "parse.h"
+# include "libft/libft.h"
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <stdlib.h>
 # include <signal.h>
 # include <stdio.h>
 # include <unistd.h>
+
+void	rl_replace_line (const char *, int);
+void	rl_clear_history (void);
 
 #endif
