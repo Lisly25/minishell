d2a21f07a3bafa5d3e559cfbfbda78be7d9440b8
Finn Shields <fshields@c1r3p6.hive.fi>
2024-02-22 17:05:36 +0200

    changing compile.sh so it doesn't touch files it shouldn't (I should really just turn this into a makefile but I'm lazy..

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-cc *.c built_ins/*.c -lreadline -L ~/.brew/opt/readline/lib libft/libft.a -o minishell
+cc read_line.c built_ins/*.c -lreadline -L ~/.brew/opt/readline/lib libft/libft.a -o minishell
