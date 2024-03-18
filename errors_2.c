1803ad20577c4ce85be94cabb354c549fea8391f
Szabina Korbai <skorbai@c1r4p5.hive.fi>
2024-03-18 12:28:49 +0200

    Put null-checks before freeing anything

--- errors_2.c
+++ errors_2.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 11:48:11 by skorbai           #+#    #+#             */
-/*   Updated: 2024/03/15 11:49:16 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/03/18 11:00:46 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,29 @@ void	ft_error_message(char *message, char *target)
 	ft_putstr_fd(target, 2);
 	ft_putstr_fd(": ", 2);
 	ft_putendl_fd(message, 2);
+}
+
+void	free_comm(t_data *data)
+{
+	int		i;
+
+	i = 0;
+	while (i < data->comm_count)
+	{
+		if (data->comms == NULL)
+			return ;
+		while (i < data->comm_count)
+		{
+			if (data->comms[i] != NULL)
+			{
+				ft_free_2d_array(data->comms[i]->redirect);
+				ft_free_2d_array(data->comms[i]->command);
+				ft_free_2d_array(data->comms[i]->san_command);
+				free(data->comms[i]);
+			}
+			i++;
+		}
+	}
+	free(data->comms);
+	data->comms = NULL;
 }
