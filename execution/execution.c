fa138b4e310bfcce3976a207d55641d45f172c56
Finn Shields <fshields@c1r4p7.hive.fi>
2024-03-21 14:37:45 +0200

    fixing small errors I just created

--- execution/execution.c
+++ execution/execution.c
@@ -6,25 +6,11 @@
 /*   By: fshields <fshields@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:51:58 by fshields          #+#    #+#             */
-/*   Updated: 2024/03/21 13:52:42 by fshields         ###   ########.fr       */
+/*   Updated: 2024/03/21 14:33:45 by fshields         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-//find a place for this vvv
-
-static int	is_n_flag(t_comm *command)
-{
-	char	*str;
-
-	str = command->san_command[1];
-	if (!str)
-		return (0);
-	if (ft_strncmp("-n", str, 2) == 0 && ft_strlen(str) == 2)
-		return (1);
-	return (0);
-}
 
 //ret of 1: not built_in
 //ret of -1: error
@@ -61,8 +47,6 @@ static int	built_in_loop(t_data *data, int i, int code)
 		i ++;
 	if (run_built_in(data->comms[0]->san_command[i], code, data, 0) != 0)
 		return (1);
-	if (data->comms[0]->san_command[i] == NULL)
-		return (0);
 	if (code == 1 && data->comms[0]->san_command[i + 1] != NULL)
 		printf(" ");
 	return (0);
@@ -82,8 +66,13 @@ static int	exec_built_in_no_exit(t_data *data)
 	code = detect_built_in(data->comms[0]->san_command[0]);
 	i = 1;
 	while (data->comms[0]->san_command[i] != NULL || i == 1)
-		if (built_in_loop(data, i++, code) == 1)
+	{
+		if (built_in_loop(data, i, code) == 1)
 			return (1);
+		if (data->comms[0]->san_command[i] == NULL)
+			break ;
+		i ++;
+	}
 	if (code == 1 && !is_n_flag(data->comms[0]))
 		printf("\n");
 	reset_io(io);
